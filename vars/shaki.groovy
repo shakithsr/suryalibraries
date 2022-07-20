55bdd5abb19c9d9bb94dc37046d30a496d30ebd6
shakithsr <108788364+shakithsr@users.noreply.github.com>
2022-07-20 18:12:26 +0530

    Update shaki.groovy

--- vars/shaki.groovy
+++ vars/shaki.groovy
@@ -7,3 +7,8 @@ def Smaven()
 {
   sh 'mvn package'
 }
+
+def Sdeploy(jobname,ip,context)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/$(jobname)/webapp/target/webapp.war ubuntu@$(ip):/var/lib/tomcat9/webwap/$(context).war"
+}
