4f9485cd71df8ba765218a61862245049a29acce
shakithsr <108788364+shakithsr@users.noreply.github.com>
2022-07-20 18:49:59 +0530

    Update shaki.groovy

--- vars/shaki.groovy
+++ vars/shaki.groovy
@@ -12,3 +12,9 @@ def Sdeploy(jobname,ip,context)
 {
   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+
+def Stest(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
+  
